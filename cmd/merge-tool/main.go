e6559f7871cab8746a82d6dd107625b05fffab4e
荒野無燈 <ttys3@outlook.com>
2021-06-03 01:44:56 +0800

    refactor: refine cmd/merge-tool/main.go

--- cmd/merge-tool/main.go
+++ cmd/merge-tool/main.go
@@ -1,40 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/deckarep/golang-set"
 	"github.com/ttys3/hugo-algolia-updater/common"
+	"os"
 	"strings"
 )
 
+var pathsWithCommaSep string
+var outfile string
+
 func main() {
-	//mergeDict()
-	mergeStop()
-}
+	flag.StringVar(&pathsWithCommaSep, "f", "", "files with comma separated to merge")
+	flag.StringVar(&outfile, "t", "out.dict", "destination file to save to")
+	flag.Parse()
 
-func mergeDict() {
-	path := "/Users/naah/software/GOPATH/src/github.com/go-ego/gse/data/dict/dictionary.txt,/Users/naah/software/GOPATH/src/github.com/go-ego/gse/data/dict/zh/dict.txt,/Users/naah/software/GOPATH/src/github.com/yanyiwu/gojieba/dict/jieba.dict.utf8"
-	split := strings.Split(path, ",")
-	set := mapset.NewSet()
-	for index1 := range split {
-		p := split[index1]
-		s := common.ReadFileString(p)
-		sa := strings.Split(s, "\r\n")
-		for index2 := range sa {
-			set.Add(sa[index2])
-			common.Num++
-		}
-
+	if pathsWithCommaSep == "" {
+		os.Exit(-1)
 	}
-	slice := set.ToSlice()
-	fmt.Print(len(slice))
-	array := common.InterfaceArray2StringArray(slice, 1)
-	str := strings.Join(array, "\r\n")
-	common.WriteFile("1.dict", []byte(str))
+
+	// example dict  "/github.com/go-ego/gse/data/dict/dictionary.txt,/github.com/go-ego/gse/data/dict/zh/dict.txt,/github.com/yanyiwu/gojieba/dict/jieba.dict.utf8"
+	// example stop words "/github.com/yanyiwu/gojieba/dict/stop_words.utf8,/Hugo/Naah-Blog/stop.txt"
+	mergeDict(pathsWithCommaSep, outfile)
 }
-func mergeStop() {
-	path := "/Users/naah/software/GOPATH/src/github.com/yanyiwu/gojieba/dict/stop_words.utf8,/Users/naah/Documents/Hugo/Naah-Blog/stop.txt"
-	split := strings.Split(path, ",")
+
+func mergeDict(pathsWithCommaSep string, outfile string) {
+	split := strings.Split(pathsWithCommaSep, ",")
 	set := mapset.NewSet()
 	for index1 := range split {
 		p := split[index1]
@@ -50,5 +43,5 @@ func mergeStop() {
 	fmt.Print(len(slice))
 	array := common.InterfaceArray2StringArray(slice, 1)
 	str := strings.Join(array, "\n")
-	common.WriteFile("stop1.txt", []byte(str))
+	common.WriteFile(outfile, []byte(str))
 }
